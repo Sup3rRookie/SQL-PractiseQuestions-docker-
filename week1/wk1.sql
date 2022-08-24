36d1bfb52655da95451ce94a87d499bc1119d816
Sup3rRookie <dev.akaun@gmail.com>
2022-08-24 18:52:42 +0800

    add new sql question & answer

--- week1/wk1.sql
+++ week1/wk1.sql
@@ -9,3 +9,15 @@ order by s.customer_id
 select customer_id , count(distinct order_date) as num_of_days
 from sales
 group by customer_id
+
+-- What was the first item from the menu purchased by each customer?
+select x.customer_id, x.product_name
+from
+  (
+    select s.customer_id, s.order_date , m.product_name,
+    DENSE_RANK() OVER(partition by customer_id ORDER BY s.order_date) AS rank
+    from sales s
+    join menu m on s.product_id = m.product_id
+  ) x
+where x.rank = 1
+group by x.customer_id, x.product_name
